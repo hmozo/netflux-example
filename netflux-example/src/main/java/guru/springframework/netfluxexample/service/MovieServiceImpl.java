e151d91036ce00a4f7a5d34d58df23f42c14457a
Hugo Mozo <hmozo@outlook.com>
2020-09-30 15:07:46 +0200

    MovieService

---
+++ netflux-example/src/main/java/guru/springframework/netfluxexample/service/MovieServiceImpl.java
@@ -0,0 +1,38 @@
+package guru.springframework.netfluxexample.service;
+
+import java.time.Duration;
+import java.util.Date;
+
+import guru.springframework.netfluxexample.domain.Movie;
+import guru.springframework.netfluxexample.domain.MovieEvent;
+import guru.springframework.netfluxexample.repositories.MovieRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class MovieServiceImpl implements MovieService {
+
+	private final MovieRepository movieRepository;
+	
+	public MovieServiceImpl(MovieRepository movieRepository) {
+		this.movieRepository= movieRepository;
+	}
+	
+	@Override
+	public Flux<MovieEvent> events(String movieId) {
+		return Flux.<MovieEvent>generate(movieEventSynchronousSink -> 
+				movieEventSynchronousSink.next(
+						new MovieEvent(movieId, new Date())))
+			.delayElements(Duration.ofSeconds(1));
+	}
+
+	@Override
+	public Mono<Movie> getMovieById(String id) {
+		return this.movieRepository.findById(id);
+	}
+
+	@Override
+	public Flux<Movie> getAllMovied() {
+		return movieRepository.findAll();
+	}
+
+}

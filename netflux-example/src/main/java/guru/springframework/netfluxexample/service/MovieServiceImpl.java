e2dc70fb830105ab457d2ba255f65e97441c9a6d
Hugo Mozo <hmozo@outlook.com>
2020-09-30 15:27:04 +0200

    MovieService change

--- netflux-example/src/main/java/guru/springframework/netfluxexample/service/MovieServiceImpl.java
+++ netflux-example/src/main/java/guru/springframework/netfluxexample/service/MovieServiceImpl.java
@@ -3,12 +3,15 @@ package guru.springframework.netfluxexample.service;
 import java.time.Duration;
 import java.util.Date;
 
+import org.springframework.stereotype.Service;
+
 import guru.springframework.netfluxexample.domain.Movie;
 import guru.springframework.netfluxexample.domain.MovieEvent;
 import guru.springframework.netfluxexample.repositories.MovieRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Service
 public class MovieServiceImpl implements MovieService {
 
 	private final MovieRepository movieRepository;

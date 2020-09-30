71ee7cc906511b919a0a99097e9dd324bf6f33ad
Hugo Mozo <hmozo@outlook.com>
2020-09-30 15:07:01 +0200

    Domain changes

--- netflux-example/src/main/java/guru/springframework/netfluxexample/domain/Movie.java
+++ netflux-example/src/main/java/guru/springframework/netfluxexample/domain/Movie.java
@@ -1,7 +1,9 @@
 package guru.springframework.netfluxexample.domain;
 
+import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -9,6 +11,7 @@ import lombok.NonNull;
 @Document
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Movie {
 	
 	private String id;
--- netflux-example/src/main/java/guru/springframework/netfluxexample/domain/MovieEvent.java
+++ netflux-example/src/main/java/guru/springframework/netfluxexample/domain/MovieEvent.java
@@ -2,11 +2,13 @@ package guru.springframework.netfluxexample.domain;
 
 import java.util.Date;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class MovieEvent {
 	private String movieId;
 	private Date date;
